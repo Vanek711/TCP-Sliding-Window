dcad0aefed0b154297dec1d8a7c95c08314545ba
Varenik711 <dubrovin_ivan_2003@mail.com>
2023-05-24 00:25:02 +0300

    Commit

--- TCP-SLIDING-WINDOW/TCPClient.cs
+++ TCP-SLIDING-WINDOW/TCPClient.cs
@@ -1,12 +1,15 @@
 ﻿using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using String = System.String;
 
 namespace ExampleTcpClient
 {
     class TCPClient
     {
-
+        private static int port = 2222; 
         static void Main(string[] args)
         {
             try
@@ -33,44 +36,38 @@ namespace ExampleTcpClient
                 Console.WriteLine("Exeption");
             }
         }
-
+        
         static async void Connect(string address,string addressName)
         {
-            Thread.Sleep(1000);
-            short port = 8080;
-            var pathDirectory = Environment.CurrentDirectory;
+            Console.WriteLine("Synchronization . . . ");
+            string messageSynchronization = "SYN";
+            Byte[] data = new byte[1024];
+
             TcpClient client = new TcpClient(address, port);
-            string messageSynchronization = "Synchronization . . . ";
-            byte[] dataSynch = System.Text.Encoding.ASCII.GetBytes(messageSynchronization);
             NetworkStream stream = client.GetStream();
+
+            byte[] dataSynch = Encoding.ASCII.GetBytes(messageSynchronization);
+
             stream.Write(dataSynch, 0, dataSynch.Length);
             Console.WriteLine("Sent: {0} ", messageSynchronization);
-            dataSynch = new byte[1024];
-            int bytes = stream.Read(dataSynch, 0, dataSynch.Length);
-            string responseData = System.Text.Encoding.ASCII.GetString(dataSynch, 0, bytes); 
+
+
+            _ = stream.ReadAsync(data, 0, data.Length);
+
+            string responseData = Encoding.ASCII.GetString(data, 0, data.Length);
+
             if (responseData != "Synchronization accepted")
             {
-                throw new Exception("No connection ");
+                Console.WriteLine("Connection Failed"); 
+                Environment.Exit(0);
             }
-            Console.WriteLine("Received: {0}", responseData);
 
-            string pathTextFile = Path.Combine(pathDirectory, @"Help\help1.doc");
-            using(StreamReader reader = new StreamReader(pathTextFile))
+            for (int i = 0; i < 2000; i++)
             {
-                string? text = await reader.ReadToEndAsync();
-                if (String.IsNullOrEmpty(text)) { throw new Exception("File is Empty."); }
+                char[] message = new char[1];
+                message[0] = (char)i;
+                stream.Write(Encoding.ASCII.GetBytes(message), 0, 1);
             }
-            byte[] data = new byte[1024];
-            data =  System.Text.Encoding.ASCII.GetBytes(text);//Из коннекта будет вызываться метод парсинга, а потом в коннекте цикл 
-            char[] textArray = String.ToCharArray(text);
-            for (int i =0; i < textArray.Length; i++)
-            {
-                stream.Write(textArray[i]);
-                Console.WriteLine("Sent: {0} ", textArray[i]);
-                if (i % 10 == 0) stream.Read(data,0,data.Length);
-
-            }
-            stream.Write(data, 0, data.Length);
 
             stream.Close();
             client.Close();
